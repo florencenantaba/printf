dcad428ee0028c676b8a4940c685d19afd7c29ce
florencenantaba <tendoprecious38@gmail.com>
2022-04-19 10:04:42 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,144 +1,90 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
-* copy_to_buffer - Copies the given character over to the buffer
-* @formatter: Character to copy over
-* @buffer: Buffer being copied to
-* @buflenptr: Pointer to the length of the buffer, the number of
-* characters in the buffer
-* @bufposptr: Pointer to the position in the buffer
-*
-* Return: Number of characters copied to buffer
-*/
-int copy_to_buffer(char formatter, char buffer[],
-		     int *buflenptr, int *bufposptr)
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
 {
-	int chars;
-
-	chars = 0;
-	buffer[*bufposptr] = formatter;
-	*bufposptr += 1;
-	*buflenptr += 1;
-	if (*buflenptr == 1024)
-	{
-		write_buffer(buffer, buflenptr, bufposptr);
-	}
-	chars++;
-	return (chars);
-}
-
-
-/**
-* check_conversion - Checks formatter character to see if
-* it's a conversion specifier
-* @formatter: The format character being checked
-* @conversions: Struct holding conversion specifiers & function pointers to
-* @appropriate functions for corresponding conversion specifier
-* @buffer: Buffer needed to copy to when calling function
-* @buflenptr: Pointer to the length of the buffer
-* @bufposptr: Pointer to the position within the buffer
-* @print_this: va_list holding all given arguments to _printf function
-*
-* Return: Return the number of characters copied to buffer if a
-* function is called, 0 if no function is called
-*/
-int check_conversion(char formatter, char_funcs_t conversions[], char buffer[],
-		     int *buflenptr, int *bufposptr, va_list print_this)
-{
-	int j, chars;
-
-	chars = 0;
-	for (j = 0; j < 13; j++)
-	{
-		if (formatter == *conversions[j].c)
-		{
-			chars += conversions[j].f(print_this, buffer, buflenptr,
-						  bufposptr);
-			return (chars);
-		}
-	}
-	return (0);
-}
-/**
-* formatPrinter - finds the formatters function and prints its arguement
-* @format: The format character being checked
-* @conversions: Struct holding conversion specifiers & function pointers to
-* appropriate functions for corresponding conversion specifier
-* @buffer: Buffer needed to copy to when calling function
-* @buflenptr: Pointer to the length of the buffer
-* @bufposptr: Pointer to the position within the buffer
-* @print_this: va_list holding all given arguments to _printf function
-*
-* Return: Return the number of characters copied to buffer if a
-* function is called, 0 if no function is called
-*/
-int formatPrinter(const char *format, va_list print_this, char buffer[],
-		  int *buflenptr, int *bufposptr, char_funcs_t conversions[])
-{
-	int i, chars, print;
-
-	chars = 0;
-	for (i = 0; format[i] != '\0' && format != NULL; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			print = check_conversion(format[i], conversions,
-						 buffer, buflenptr, bufposptr,
-						 print_this);
-			if (print == 0)
-				chars += copy_to_buffer(format[i], buffer,
-							buflenptr, bufposptr);
-			chars += print;
-		}
-		else
-		{
-			chars += copy_to_buffer(format[i], buffer, buflenptr,
-						bufposptr);
-		}
-	}
-	return (chars);
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
 /**
-* _printf - Print out a formatted string
-* @format: Format of the string
-* Return: Number of characters printed
-*/
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-	va_list print_this;
-	char buffer[1024];
-	int chars, buflen, bufpos, *buflenptr, *bufposptr;
-	char_funcs_t conversions[] = {{"c", print_c},
-				      {"s", print_s},
-				      {"i", print_int},
-				      {"d", print_int},
-				      {"u", print_u},
-				      {"o", print_o},
-				      {"x", print_hex},
-				      {"X", print_heX},
-				      {"b", print_b},
-				      {"S", print_S},
-				      {"r", print_r},
-				      {"R", print_R},
-				      {"p", print_p},
-	};
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-	initialize_buffer(buffer);
-	chars = bufpos = 0;
-	buflen = 1;
-	buflenptr = &buflen;
-	bufposptr = &bufpos;
-	va_start(print_this, format);
-	if (format == NULL || print_this == NULL)
-		return (chars);
-	chars = formatPrinter(format, print_this, buffer,
-			      buflenptr, bufposptr, conversions);
-	write_buffer(buffer, buflenptr, bufposptr);
-	va_end(print_this);
-	return (chars);
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
--- main.h
+++ main.h
@@ -1,37 +1,116 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
-/**
- * struct char_funcs - Struct to find function based on char
- * @c: char to check as key
- * @f: Pointer to function to call
- */
-typedef struct char_funcs
-{
-	char *c;
-	int (*f)();
-} char_funcs_t;
+#include <unistd.h>
 
-void write_buffer(char buffer[], int *buflen, int *bufpos);
-void initialize_buffer(char buffer[]);
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
+
+/**
+ * struct buffer_s - A new type defining a buffer struct.
+ * @buffer: A pointer to a character array.
+ * @start: A pointer to the start of buffer.
+ * @len: The length of the string stored in buffer.
+ */
+typedef struct buffer_s
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
+
+/**
+ * struct converter_s - A new type defining a converter struct.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+} converter_t;
+
+/**
+ * struct flag_s - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
+ */
+typedef struct flag_s
+{
+	unsigned char flag;
+	unsigned char value;
+} flag_t;
+
 int _printf(const char *format, ...);
-int print_c(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_s(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_number(int n, char buffer[], int *buflen, int *bufpos);
-int print_int(va_list args, char buffer[], int *buflen, int *bufpos);
-char *binConverter(unsigned int n);
-char *octConverter(unsigned int n);
-char *hexConverter(char type, unsigned int n);
-char *size_tHex(char type, size_t n);
-int print_Unum(unsigned int n, char buffer[], int *buflen, int *bufpos);
-int print_u(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_o(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_hex(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_heX(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_b(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_S(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_r(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_R(va_list args, char buffer[], int *buflen, int *bufpos);
-int print_p(va_list args, char buffer[], int *buflen, int *bufpos);
-#endif
+
+/* Conversion Specifier Functions */
+unsigned int convert_c(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_s(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_di(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_percent(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_b(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_u(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_o(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_x(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_X(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_S(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_p(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_r(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_R(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len);
+
+/* Handlers */
+unsigned char handle_flags(const char *flags, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
+
+/* Modifiers */
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+
+/* Helper Functions */
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *output);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
+
+#endif /* MAIN_H */
